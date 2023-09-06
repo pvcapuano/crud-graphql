7959a5f8e7d4fcbc4775b3a545858e1b670b09b1
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-09-06 20:14:01 -0300

    feat: updatePassword

---
+++ server/queries.ts
@@ -0,0 +1,15 @@
+/* query{
+    getAllUsers{
+        id
+        name
+        username
+        password
+    }
+}
+
+mutation{
+    updatePassword(username:"pvcapuano", oldPassword:"teste", newPassword:"hey"){
+      successful
+      message
+    }
+  } */
